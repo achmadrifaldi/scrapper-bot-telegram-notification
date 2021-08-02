b357a75855bc715b9e83dd61adfd3893df88207d
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2021-08-02 15:39:38 +0700

    Separate scheduler and bot

--- main.rb
+++ main.rb
@@ -1,23 +1,19 @@
 require 'telegram/bot'
 require 'yaml'
-require_relative 'scheduler'
 
 # Read config from yaml file
 config = YAML.load_file('config.yml')
 TOKEN = config['config']['token'] ||= ""
-URLS = config['config']['urls'] ||= []
-TIMER = config['config']['scheduler'] ||= '15m'
-
-# Run scheduler
-Scheduler.init(URLS, TIMER)
 
 # Register chat id into file
 Telegram::Bot::Client.run(TOKEN) do |bot|
     bot.listen do |message|
         case message.text
         when '/start'
-            File.write('chat.txt', message.chat.id, mode: 'a')
+            File.write('chat.txt', "#{message.chat.id}\n", mode: 'a')
             bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}.")
+        when '/info'
+            bot.api.send_message(chat_id: message.chat.id, text: "Bot is running! use /start to start run")
         else
         end
     end
--- scheduler.rb
+++ scheduler.rb
@@ -1,5 +1,6 @@
 require_relative 'scraper'
 require 'rufus-scheduler'
+require 'yaml'
 
 class Scheduler
     def self.init(urls, timer = '15m')
@@ -14,3 +15,10 @@ class Scheduler
         scheduler.join
     end
 end
+
+config = YAML.load_file('config.yml')
+URLS = config['config']['urls'] ||= []
+TIMER = config['config']['scheduler'] ||= '15m'
+
+# Run scheduler
+Scheduler.init(URLS, TIMER)
